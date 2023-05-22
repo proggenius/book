cf44068d6c8f4908114e64c1dd3615d3e9d3f667
proggenius <133808488+proggenius@users.noreply.github.com>
2023-05-22 14:42:16 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function toggle() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
